a71f3531b435d608991b071737e0fed64f7e401c
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-06 10:28:17 +0700

    update portfolio page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,62 @@
-import React from "react";
-import LoginForm from "../components/LoginForm";
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
-const Login = () => {
+const LoginPage = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("Logging in with:", username, password);
+  };
+
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <h2 className="text-white text-4xl font-bold mb-6 text-center">
-        Crypto Portfolio Manager
-      </h2>
-      <div className="bg-[#232323] border p-8 rounded w-full max-w-sm">
-        <h2 className="text-white text-2xl font-bold mb-6 text-center">
-          Login
-        </h2>
-        <LoginForm />
+    <div className="all-center pt-10">
+      <div className="w-96">
+        <p className="text-4xl">Welcome</p>
+        <p className="text-[#7c7c7c]">Login to your account</p>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <p className="text-[#7c7c7c]">Username</p>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="w-96 placeholder-[#7c7c7c] p-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-[#7c7c7c]">Password</p>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-96 placeholder-[#7c7c7c] p-2 hover:text-hoverYellow"
+            />
+          </div>
+          <button className="primary w-96" type="submit">
+            Login
+          </button>
+        </form>
+        <br />
+        <p className="text-[#7c7c7c] all-center">
+          Don&apos;t have an account?
+          <span className="ml-2 text-white underline">
+            <Link href="/register">Register Now</Link>
+          </span>
+        </p>
+        <br />
       </div>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,21 +1,73 @@
 "use client";
-import React from "react";
-import RegisterForm from "../components/RegisterForm";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
-const Register = () => {
+const LoginPage = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("register in with:", username, password, confirmPassword);
+  };
+
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <h2 className="text-white text-4xl font-bold mb-6 text-center">
-        Crypto Portfolio Manager
-      </h2>
-      <div className="bg-[#232323] border p-8 rounded w-full max-w-sm">
-        <h2 className="text-white text-2xl font-bold mb-6 text-center">
-          Register
-        </h2>
-        <RegisterForm />
+    <div className="all-center pt-10">
+      <div className="w-96">
+        <p className="text-4xl">Get start</p>
+        <p className="text-[#7c7c7c]">Create a new account</p>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <p className="text-[#7c7c7c]">Username</p>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="w-96 placeholder-[#7c7c7c] p-2"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-[#7c7c7c]">Password</p>
+            <input
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-96 placeholder-[#7c7c7c] p-2 hover:text-hoverYellow"
+            />
+          </div>
+          <div className="mb-4">
+            <p className="text-[#7c7c7c]">Confirm Password</p>
+            <input
+              type="password"
+              placeholder="Enter confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              className="w-96 placeholder-[#7c7c7c] p-2 hover:text-hoverYellow"
+            />
+          </div>
+          <button className="primary w-96" type="submit">
+            Register
+          </button>
+        </form>
+        <br />
+        <p className="text-[#7c7c7c] all-center">
+          Have an account?
+          <span className="ml-2 text-white underline">
+            <Link href="/login">Login Now</Link>
+          </span>
+        </p>
       </div>
     </div>
   );
 };
 
-export default Register;
+export default LoginPage;
